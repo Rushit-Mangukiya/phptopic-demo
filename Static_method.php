0fc0fbf30761e0c60c8ff9b8f4b49552b22f2dbd
Rushit-Mangukiya <developer@webmigrates.com>
2023-10-12 12:19:01 +0530

    change the code

--- Static_method.php
+++ Static_method.php
@@ -1,24 +1,32 @@
 <?php
     
     class hello{
-      public static $name = "hello rushit mangukiya";
-      public static $age = "your age is : 20";
+      public static $name = "my name is rushit mangukiya";
+      public static $age = "My Age is : 20";
 
       public static function intro(){
         echo self::$name;
         echo"<br>";
         echo self::$age;
       }
+        public function __construct($n ,$x){
+          self::$name = $n;
+          self::$age = $x;
+        }
     }
+      
 
-    echo hello::$name;
-    echo"<br>";
-    echo hello::$age;
-    echo"<br>";
-    hello::intro();
-    echo"<br>";
-    hello::intro();
+    // echo hello::$name;
+    // echo"<br>";
+    // echo hello::$age;
+    // echo"<br>";
+    // hello::intro();
+    // echo"<br>";
+    // hello::intro();
     
+    $obj = new hello("hello rushit",20);
+    //   echo"<br>";
+   echo $obj->intro();
 
 
 
